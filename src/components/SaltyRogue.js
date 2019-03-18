4cb7e73b0897e041861c5719230c0dab0980b6d0
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-03-18 16:08:13 -0600

    second commit

---
+++ src/components/SaltyRogue.js
@@ -0,0 +1,12 @@
+import { SerialNumber } from "../assets/SerialNumbers.json";
+
+var url = require('url')
+
+function getAddress(){
+    fetch('http://www.utahcounty.gov/LandRecords/Property.asp?av_serial=' + '{{SerialNumber}}')
+    .then((this.SerialNumber){
+        return;
+    })
+    .then return
+
+http://www.utahcounty.gov/LandRecords/Property.asp?av_serial=10670048
